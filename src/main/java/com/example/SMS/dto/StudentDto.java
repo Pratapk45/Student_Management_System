3970b22b7f300d0449855603a20d8a4fd6232dd0
Pratapk45 <pratapkachare45@gmail.com>
2025-01-09 08:14:09 +0530

    git commit - "Student management project"

---
+++ src/main/java/com/example/SMS/dto/StudentDto.java
@@ -0,0 +1,81 @@
+package com.example.SMS.dto;
+
+import lombok.Data;
+
+
+
+@Data
+public class StudentDto {
+    private String studFirstName;
+    private String studLastName;
+    private String email;
+    private String gender;
+    private String address;
+    private String dOB;
+    private String joining_Date;
+    private Integer mobile_no;
+
+    public String getStudFirstName() {
+        return studFirstName;
+    }
+
+    public void setStudFirstName(String studFirstName) {
+        this.studFirstName = studFirstName;
+    }
+
+    public String getStudLastName() {
+        return studLastName;
+    }
+
+    public void setStudLastName(String studLastName) {
+        this.studLastName = studLastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getdOB() {
+        return dOB;
+    }
+
+    public void setdOB(String dOB) {
+        this.dOB = dOB;
+    }
+
+    public String getJoining_Date() {
+        return joining_Date;
+    }
+
+    public void setJoining_Date(String joining_Date) {
+        this.joining_Date = joining_Date;
+    }
+
+    public Integer getMobile_no() {
+        return mobile_no;
+    }
+
+    public void setMobile_no(Integer mobile_no) {
+        this.mobile_no = mobile_no;
+    }
+}
