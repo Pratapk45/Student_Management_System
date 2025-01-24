8d1f3769d10ed2638b0ac402a90693896f2175ab
Pratapk45 <pratapkachare45@gmail.com>
2025-01-24 09:19:28 +0530

    Project added

---
+++ src/main/java/com/example/SMS/controller/studentController.java
@@ -0,0 +1,65 @@
+package com.example.SMS.controller;
+
+import com.example.SMS.dto.StudentDto;
+import com.example.SMS.entity.StudentEntity;
+import com.example.SMS.services.StudentServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/student")
+public class studentController
+{
+
+    @Autowired
+    private StudentServices studentServices;
+
+    @PostMapping("/studadd")
+    public  ResponseEntity<StudentEntity> createStudent(@RequestBody StudentDto studentDto)
+    {
+
+        return ResponseEntity.ok(studentServices.createStudent(studentDto));
+        //OR
+        /*studentServices.createStudent(studentDto);
+        return  "Student added";*/
+    }
+
+    @GetMapping("/getStudent")
+    public ResponseEntity<List<StudentEntity>> getAllStudentRecord()
+    {
+        List<StudentEntity> list=studentServices.getAllStudentRecord();
+        return ResponseEntity.ok(list);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable Long id)
+    {
+        studentServices.deleteStudent(id);
+        return  ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StudentEntity> updateStudent(@PathVariable Long id, @RequestBody StudentDto studentDto)
+    {
+        return ResponseEntity.ok(studentServices.updateStudent(id,studentDto));
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public Optional<StudentEntity> getStudentById(@PathVariable Long id)
+    {
+       return studentServices.getStudentById(id);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<StudentEntity> updateStudentPartially(@PathVariable Long id, @RequestBody StudentDto studentDto)
+    {
+        return ResponseEntity.ok(studentServices.updateStudentPartially(id,studentDto));
+
+    }
+
+}
---
+++ src/main/java/com/example/SMS/services/StudentServices.java
@@ -0,0 +1,85 @@
+package com.example.SMS.services;
+
+import com.example.SMS.dto.StudentDto;
+import com.example.SMS.entity.StudentEntity;
+import com.example.SMS.repository.StudentRepo;
+import org.hibernate.dialect.unique.CreateTableUniqueDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServices {
+    @Autowired
+    private StudentRepo studentRepo;
+
+    //Create Student entry
+    public StudentEntity createStudent(StudentDto studentDto) {
+        StudentEntity studentEntity = new StudentEntity();
+        //studentEntity.setStudId(0);
+        studentEntity.setStudFirstName(studentDto.getStudFirstName());
+        studentEntity.setStudLastName(studentDto.getStudLastName());
+        studentEntity.setEmail(studentDto.getEmail());
+        studentEntity.setGender(studentDto.getGender());
+        studentEntity.setAddress(studentDto.getAddress());
+        studentEntity.setdOB(studentDto.getdOB());
+        studentEntity.setJoining_Date(studentDto.getJoining_Date());
+        studentEntity.setMobile_no(studentDto.getMobile_no());
+        studentRepo.save(studentEntity);
+        return studentEntity;
+
+    }
+
+    //get all student details list
+    public List<StudentEntity> getAllStudentRecord() {
+        return studentRepo.findAll();
+    }
+
+    //delete student entry
+    public void deleteStudent(Long id) {
+        studentRepo.deleteById(id);
+    }
+
+
+    //Update Student data
+    public StudentEntity updateStudent(Long id, StudentDto studentDto) {
+
+        StudentEntity existingdata = studentRepo.findById(id).orElseThrow(() -> new RuntimeException("student not found"));
+
+        existingdata.setStudFirstName(studentDto.getStudFirstName());
+        existingdata.setStudLastName(studentDto.getStudLastName());
+        existingdata.setEmail(studentDto.getEmail());
+        existingdata.setGender(studentDto.getGender());
+        existingdata.setAddress(studentDto.getAddress());
+        existingdata.setdOB(studentDto.getdOB());
+        existingdata.setJoining_Date(studentDto.getJoining_Date());
+        existingdata.setMobile_no(studentDto.getMobile_no());
+        return studentRepo.save(existingdata);
+
+
+    }
+
+    //Get SingleRecord of student from database
+    public Optional<StudentEntity> getStudentById(Long id) {
+        return studentRepo.findById(id);
+    }
+
+    public StudentEntity updateStudentPartially(Long id, StudentDto studentDto) {
+        StudentEntity existingdata = studentRepo.findById(id).orElseThrow(() -> new RuntimeException("student not found"));
+
+
+//        existingdata.setStudFirstName(studentDto.getStudFirstName());
+//        existingdata.setStudLastName(studentDto.getStudLastName());
+//        existingdata.setEmail(studentDto.getEmail());
+       // existingdata.setGender(studentDto.getGender());
+        existingdata.setAddress(studentDto.getAddress());
+//existingdata.setdOB(studentDto.getdOB());
+//        existingdata.setJoining_Date(studentDto.getJoining_Date());
+//        existingdata.setMobile_no(studentDto.getMobile_no());
+        return studentRepo.save(existingdata);
+    }
+
+
+}
